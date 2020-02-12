48e71387e0a9eede2c3ac5e9da287d4dc9ed741f
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-02-12 15:39:45 +0300

    конечные изменения

--- js/main.js
+++ js/main.js
@@ -1,65 +1,80 @@
 'use strict';
 
 var AD_AVATARS = [
-  'img/avatars/user01.png',
-  'img/avatars/user02.png',
-  'img/avatars/user03.png',
-  'img/avatars/user04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png'
+  "img/avatars/user01.png",
+  "img/avatars/user02.png",
+  "img/avatars/user03.png",
+  "img/avatars/user04.png",
+  "img/avatars/user05.png",
+  "img/avatars/user06.png",
+  "img/avatars/user07.png",
+  "img/avatars/user08.png"
 ];
 
 var AD_TITLES = [
-  'Белая резиденция',
-  'Чёрная резиденция',
-  'Красная резиденция',
-  'Золотая резиденция',
-  'Зелёная резиденция',
-  'Фиолетовая резиденция',
-  'Салатовая резиденция',
-  'Жёлтая резиденция'
+  "Белая резиденция",
+  "Чёрная резиденция",
+  "Красная резиденция",
+  "Золотая резиденция",
+  "Зелёная резиденция",
+  "Фиолетовая резиденция",
+  "Салатовая резиденция",
+  "Жёлтая резиденция"
 ];
 
-var AD_TYPES = ['дворец', 'квартира', 'дом', 'бунгало'];
+var AD_TYPES = [
+  "дворец",
+  "квартира",
+  "дом",
+  "бунгало"
+];
 
-var AD_ROOMS = [1, 2, 3, 100];
+var AD_ROOMS = [
+  1,
+  2,
+  3,
+  100
+];
 
-var AD_TIMES = ['12:00', '13:00', '14:00'];
+var AD_TIMES = [
+  "12:00",
+  "13:00",
+  "14:00"
+];
 
 var AD_FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner'
+  "wifi",
+  "dishwasher",
+  "parking",
+  "washer",
+  "elevator",
+  "conditioner"
 ];
 
 var AD_PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+  "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
 ];
 
 var AD_DESCRIPTIONS = [
-  'светлые комнаты',
-  'огромные кровати',
-  'каменные полы',
-  'тёмные стены',
-  'неуютная мебель',
-  'ванная-банная',
-  'деньги на ветер',
-  'второй срок'
+  "светлые комнаты",
+  "огромные кровати",
+  "каменные полы",
+  "тёмные стены",
+  "неуютная мебель",
+  "ванная-банная",
+  "деньги на ветер",
+  "второй срок"
 ];
+
+
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 var similarMapElement = map.querySelector('#pin').content.querySelector('.map__pins');
 var similarMapTemplate = document.querySelector('#card').content.querySelector('.map__card');
-
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -83,7 +98,7 @@ function getRandomSliceFromArray(arr) {
 }
 
 function generateLocation() {
-  var width = document.querySelector('.map').offsetWidth;
+  var width = document.querySelector(".map").offsetWidth;
   return {
     'x': getRandomInt(0, width),
     'y': getRandomInt(130, 630)
@@ -143,6 +158,7 @@ for (var i = 0; i < ads.length; i++) {
 
 similarMapElement.style.left = location.x + 32;
 similarMapElement.style.top = location.y + (65 + 22);
-similarMapElement.src = ad.offer.avatar;
-similarMapElement.alt = ad.offer.title;
+similarMapElement.src = ad.avatar;
+similarMapElement.alt = ad.title;
 similarMapElement.appendChild(fragment);
+
